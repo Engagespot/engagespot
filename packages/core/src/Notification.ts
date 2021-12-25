6125ecc0c4617fe0653f238bbea2579bfe3d78ec
Anand S <anand@logidots.com>
2021-12-25 21:08:37 +0530

    feat: added notification fetch api

--- packages/core/src/Notification.ts
+++ packages/core/src/Notification.ts
@@ -27,6 +27,10 @@ export default class Notification implements NotificationItem {
     this.createdAt = options.createdAt;
   }
 
+  /**
+   * Marks this notification as clicked
+   * @returns 
+   */
   async markAsClicked() {
     this._client.eventListenerStore?.NOTIFICATION_CLICKED.forEach(handler => {
       handler(this);
@@ -49,6 +53,52 @@ export default class Notification implements NotificationItem {
       const response = await sendRequest(options);
 
       if (response) return this;
+
+      return false;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  /**
+   * Fetches this notification & also marks this notification as seen
+   * @returns 
+   */
+  async fetch() {
+    this._client.eventListenerStore?.NOTIFICATION_CLICKED.forEach(handler => {
+      handler(this);
+    });
+
+    const options: apiRequestOptions = {
+      url: this._client.baseURL + '/notifications/' + this.id,
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-ENGAGESPOT-API-KEY': this._client.apiKey,
+        'X-ENGAGESPOT-USER-ID': this._client.userId,
+        ...(this._client.userSignature && {
+          'X-ENGAGESPOT-USER-SIGNATURE': this._client.userSignature,
+        }),
+      },
+    };
+
+    try {
+      const response = await sendRequest(options);
+
+      if (response){ 
+        
+        //We should update this object with the notification details received from API
+        this.title = response.data.title;
+        this.message = response.data.message;
+        this.icon = response.data.icon;
+        this.url = response.data.url;
+        this.seenAt = response.data.seenAt;
+        this.clickedAt = response.data.clickedAt;
+        this.createdAt = response.data.createdAt;
+
+        return this;
+
+      }
 
       return false;
     } catch (error) {
