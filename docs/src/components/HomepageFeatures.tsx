3757d5b44ae689c1e4e0b27b16139f2d8d019608
Anand S <anand@engagespot.co>
2022-09-25 21:58:47 +0530

    docs: updated POST category spec

--- docs/src/components/HomepageFeatures.tsx
+++ docs/src/components/HomepageFeatures.tsx
@@ -6,6 +6,7 @@ type FeatureItem = {
   title: string;
   image: string;
   description: JSX.Element;
+  url?: string;
 };
 
 const FeatureList: FeatureItem[] = [
@@ -18,6 +19,7 @@ const FeatureList: FeatureItem[] = [
         easy as possible to integrate with your product.
       </>
     ),
+    url: '#',
   },
   {
     title: 'React.js SDK',
@@ -28,19 +30,23 @@ const FeatureList: FeatureItem[] = [
         a beautiful Notification Center.
       </>
     ),
+    url: '#',
   },
   {
     title: 'REST API',
     image: '/img/undraw_docusaurus_react.svg',
     description: <>Use our REST API to send notifications to your users..</>,
+    url: '/docs/rest-api',
   },
 ];
 
-function Feature({ title, image, description }: FeatureItem) {
+function Feature({ title, image, description, url }: FeatureItem) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
-        <img className={styles.featureSvg} alt={title} src={image} />
+        <a href={url}>
+          <img className={styles.featureSvg} alt={title} src={image} />
+        </a>
       </div>
       <div className="text--center padding-horiz--md">
         <h3>{title}</h3>
