79fd8523d5bfc4b41221cfc351aac60dbbd56fef
hemand.nair <hemanditwiz@gmail.com>
2022-10-24 14:22:29 +0530

    chore: add example for vue

---
+++ examples/examples-vue/src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <div ref="bellIcon">Notifications</div>
+</template>
+
+<script type="module">
+import { render } from "@engagespot/client";
+
+export default {
+  name: "Notifications",
+  mounted: function () {
+    const options = {
+      apiKey: "od9t6x45udt1m3g0nznag",
+      userId: "hemanditwiz@gmail.com",
+    };
+    render(this.$refs.bellIcon, options);
+  },
+};
+</script>
